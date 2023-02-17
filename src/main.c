e4f0a72f79b3646c23f149d8eac9c02e22b896c7
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-17 14:47:28 -0300

    feat: Store users input

--- src/main.c
+++ src/main.c
@@ -20,5 +20,25 @@ int main()
   char usage_examples[MAX_INPUT_SIZE];
   char license_and_copyright_information[MAX_INPUT_SIZE];
   char final_output[MAX_INPUT_SIZE * 5];
+
+  printf("Project title: \n");
+  fgets(project_title, sizeof(project_title), stdin);
+
+  printf("About the project dependencies: \n");
+  fgets(project_dependencies, sizeof(project_dependencies), stdin);
+
+  printf("Project requirements: \n");
+  fgets(project_requirements, sizeof(project_requirements), stdin);
+
+  printf("List some usage examples: \n");
+  fgets(usage_examples, sizeof(usage_examples), stdin);
+
+  printf("License and copyright information: \n");
+  fgets(
+      license_and_copyright_information,
+      sizeof(license_and_copyright_information),
+      stdin
+  );
+
   return 0;
 }
