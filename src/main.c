e2016e3bf2e9d90542d5245a75fe97445d4629c4
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-17 14:47:07 -0300

    feat: Add new variables to store user input

--- src/main.c
+++ src/main.c
@@ -14,5 +14,11 @@ void makeAPIrequest(char prompt[MAX_INPUT_SIZE * 5])
 
 int main()
 {
+  char project_title[100];
+  char project_dependencies[MAX_INPUT_SIZE];
+  char project_requirements[MAX_INPUT_SIZE];
+  char usage_examples[MAX_INPUT_SIZE];
+  char license_and_copyright_information[MAX_INPUT_SIZE];
+  char final_output[MAX_INPUT_SIZE * 5];
   return 0;
 }
