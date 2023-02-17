cd95849b542c505b55034e23aa206b419bb93d83
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-17 20:00:50 -0300

    feat: Add new variable to store new user input

--- src/main.c
+++ src/main.c
@@ -93,6 +93,7 @@ int main()
 {
   char project_title[100];
   char project_dependencies[MAX_INPUT_SIZE];
+  char project_intentions[MAX_INPUT_SIZE];
   char project_requirements[MAX_INPUT_SIZE];
   char usage_examples[MAX_INPUT_SIZE];
   char license_and_copyright_information[MAX_INPUT_SIZE];
