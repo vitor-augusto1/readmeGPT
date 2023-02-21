010ebd5c8cc7ba01eb218d3681e02e4476663df9
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-21 12:23:27 -0300

    feat: Add user guide to prompt

--- src/main.c
+++ src/main.c
@@ -14,15 +14,19 @@ int main()
   char project_intentions[MAX_INPUT_SIZE];
   char project_requirements[MAX_INPUT_SIZE];
   char usage_examples[MAX_INPUT_SIZE];
+  char user_guide[MAX_INPUT_SIZE];
   char license_and_copyright_information[MAX_INPUT_SIZE];
-  char final_output[MAX_INPUT_SIZE * 100];
+  char final_output[MAX_INPUT_SIZE * 150];
 
   printf("\nPlease, provide as much information as you can for a very detailed readme\n");
-  printf("\nTell me a little bit about this project's functionalities: \n");
+  printf("\nWhat is this project for ?  \n");
   fgets(project_intentions, sizeof(project_intentions), stdin);
 
   printf("Project title: \n");
   fgets(project_title, sizeof(project_title), stdin);
+
+  printf("User guide (what the user have to do to run it): \n");
+  fgets(user_guide, sizeof(user_guide), stdin);
 
   printf("About the project dependencies (What you've used to create this project): \n");
   fgets(project_dependencies, sizeof(project_dependencies), stdin);
@@ -51,6 +55,8 @@ int main()
   strcat(final_output, project_title);
   strcat(final_output, "project dependencies-> ");
   strcat(final_output, project_dependencies);
+  strcat(final_output, "user guide-> ");
+  strcat(final_output, user_guide);
   strcat(final_output, "project requirements-> ");
   strcat(final_output, project_requirements);
   strcat(final_output, "usage examples-> ");
@@ -58,10 +64,6 @@ int main()
   strcat(final_output, "project license and copyright-> ");
   strcat(final_output, license_and_copyright_information);
 
-  for (int i = 0; i < strlen(final_output); i++) {
-    if (final_output[i] == '\n')
-      final_output[i] = ' ';
-  }
   remove_new_lines(final_output);
 
   // Parse the JSON response
