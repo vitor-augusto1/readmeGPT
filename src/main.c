a53383032f61b09f2892158dce6546c91c9cf614
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-21 12:59:38 -0300

    style: Move `remove_new_lines` function to lib directory

--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 #include <unistd.h>
 #include "includes/request.h"
 
-void remove_new_lines(char *string_pointer);
-
 int main()
 {
   char project_title[100];
@@ -88,12 +86,4 @@ int main()
   }
 
   return 0;
-}
-
-void remove_new_lines(char *string_pointer)
-{
-  for (int i = 0; i < strlen(string_pointer); i++){
-    if (string_pointer[i] == '\n')
-      string_pointer[i] = ' ';
-  }
 }
