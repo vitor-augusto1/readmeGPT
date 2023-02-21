d9945a5dc699a522b347f6086d6930e9b1be6858
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-21 15:44:46 -0300

    refactor: Move the json parser to its own function and file

---
+++ src/includes/parseJSON.h
@@ -0,0 +1,11 @@
+#ifndef PARSEJSON_H
+#define PARSEJSON_H
+
+#include <json-c/json_object.h>
+#include <json-c/json.h>
+#include <json-c/json_tokener.h>
+
+const char *extract_the_text_from_api_response_json(char *text_to_parse);
+
+
+#endif
---
+++ src/lib/parseJSON.c
@@ -0,0 +1,17 @@
+#include "../includes/parseJSON.h"
+
+
+const char* extract_the_text_from_api_response_json(char *text_to_parse)
+{
+  json_object *root = json_tokener_parse(text_to_parse);
+  json_object *choices;
+  const char *extracted_text;
+  if (json_object_object_get_ex(root, "choices", &choices)) {
+    struct json_object *choice = json_object_array_get_idx(choices, 0);
+    struct json_object *text;
+    json_object_object_get_ex(choice, "text", &text);
+    extracted_text = json_object_get_string(text);
+    return extracted_text;
+  }
+  return NULL;
+}
