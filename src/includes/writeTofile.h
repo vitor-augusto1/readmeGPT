ba36f00c39a6eeeb2d7714cd9b5d0e7f9286c411
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-21 17:40:48 -0300

    feat: Add new writeTofile header

---
+++ src/includes/writeTofile.h
@@ -0,0 +1,12 @@
+#ifndef WRITETOFILE_H
+#define WRITETOFILE_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void write_to_a_file(const char *text_to_write, char file_name[]);
+
+
+#endif
