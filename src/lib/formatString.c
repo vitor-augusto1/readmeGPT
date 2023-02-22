2f8ce68e7dbc91c7a4700e67ac410aea9a7a6b97
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-22 10:59:16 -0300

    feat: Add new `remove_duplicate_slash` function to remove user duplicate `/` inputs

--- src/lib/formatString.c
+++ src/lib/formatString.c
@@ -7,3 +7,11 @@ void remove_new_lines(char *string_pointer)
       string_pointer[i] = ' ';
   }
 }
+
+void remove_duplicate_slash(char *string_pointer)
+{
+  for (int i = 0; i < strlen(string_pointer); i++){
+    if (string_pointer[i] == '/' && string_pointer[i+1] == '/')
+      string_pointer[i+1] = '\0';
+  }
+}
