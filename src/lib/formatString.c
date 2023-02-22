22b93128805e984c2cfcf88e2502dec86a6237c8
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-22 10:58:43 -0300

    feat: Remove `\` character from the final user prompt

--- src/lib/formatString.c
+++ src/lib/formatString.c
@@ -3,7 +3,7 @@
 void remove_new_lines(char *string_pointer)
 {
   for (int i = 0; i < strlen(string_pointer); i++){
-    if (string_pointer[i] == '\n')
+    if (string_pointer[i] == '\n' || string_pointer[i] == '\\')
       string_pointer[i] = ' ';
   }
 }
